d0e6752240a401f20ba9e788f50bee81830b78fa
grudev32 <snake82211432@protonmail.com>
2022-04-02 22:02:02 +0200

    Finish home services section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,45 @@ const Home = () => {
     return (
         <section className="home-route">
             <Header />
-            {/* <h1>Home Page</h1> */}
+
+            <div className="home-services">
+                <div className="services-wrapper">
+                    <h1 className="services-title">Our Services</h1>
+
+                    <div className="services-cards">
+                        <div className="service-card">
+                            <div className="card-wrapper">
+                                <img
+                                    src="https://hips.hearstapps.com/ver.h-cdn.co/assets/16/34/best-restaurants-soleil.jpg"
+                                    alt="service"
+                                    className="card-image"
+                                />
+                                <h2 className="card-title">Luxury Style</h2>
+                            </div>
+                        </div>
+                        <div className="service-card">
+                            <div className="card-wrapper">
+                                <img
+                                    src="https://www.thegibsonhotel.ie/wp-content/uploads/2019/07/couple-enjoying-romantic-dinner-restaurant-768x510.jpg"
+                                    alt="service"
+                                    className="card-image"
+                                />
+                                <h2 className="card-title">Quiet Place</h2>
+                            </div>
+                        </div>
+                        <div className="service-card">
+                            <div className="card-wrapper">
+                                <img
+                                    src="https://krispitech.com/wp-content/uploads/2021/07/Scan-a-QR-code-on-android.jpg"
+                                    alt="service"
+                                    className="card-image"
+                                />
+                                <h2 className="card-title">QR Code Menu</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
