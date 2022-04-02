7f3648581746050d986b17b690f1b94e56e3175d
grudev32 <snake82211432@protonmail.com>
2022-04-02 15:50:37 +0200

    Update Image resources and clean register route

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,26 +1,56 @@
-import { logDOM } from "@testing-library/react";
 import React from "react";
+
+import LogoImage from "../resources/images/Logo.png";
+import PizzaImage from "../resources/images/Pizza.png";
+import PastaImage from "../resources/images/Pasta.png";
 
 const Register = () => {
     return (
         <section className="register-route">
-            <div className="Registeration">
-                <img className="Logo" src={require('../imgs/Logo.png').default} alt="Logo"/>
-                <img className="Piza" src={require('../imgs/Piza.png').default} alt="Piza"/>
-                <img className="Pasta" src={require('../imgs/Pasta.png').default} alt="Pasta"/>
+            <div className="Registration">
+                <img className="Logo" src={LogoImage} alt="Logo" />
+                <img className="Pizza" src={PizzaImage} alt="Pizza" />
+                <img className="Pasta" src={PastaImage} alt="Pasta" />
 
-                <p className="paragraph1">That's from our glad to be a part of our family</p>
+                <p className="paragraph1">
+                    That's from our glad to be a part of our family
+                </p>
                 <form className="Input">
-                    <input className="input" type="text" placeholder="Full Name" required/><br/>
-                    <input className="input" type="email" placeholder="Email Address" required/><br/>
-                    <input className="input" type="password" placeholder="Password" required/><br/>
-                    <input className="input" type="password" placeholder="Confirm Password" required/><br/>
-                    <input className="Button" type="submit" value="Sign up"/>
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Full Name"
+                        required
+                    />
+                    <br />
+                    <input
+                        className="input"
+                        type="email"
+                        placeholder="Email Address"
+                        required
+                    />
+                    <br />
+                    <input
+                        className="input"
+                        type="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <br />
+                    <input
+                        className="input"
+                        type="password"
+                        placeholder="Confirm Password"
+                        required
+                    />
+                    <br />
+                    <input className="Button" type="submit" value="Sign up" />
                 </form>
 
                 <div className="form2">
                     <p>Are you already a part of our family ..?</p>
-                    <input className="Button" type="submit" value="Log In"/><br/>
+                    <input className="Button" type="submit" value="Log In" />
+                    <br />
                 </div>
             </div>
         </section>
