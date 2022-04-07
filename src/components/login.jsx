1343515a9b7b0f79b673f67da1689719a332cfde
grudev32 <snake82211432@protonmail.com>
2022-04-07 22:22:09 +0200

    Accept main branch merge changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,60 @@
 import React from "react";
 
-const Login = () => {
+import { useHistory } from "react-router-dom";
+
+import LogoImage from "../resources/Logo/Logo.png";
+import PizzaImage from "../resources/Logo/Pizza.png";
+import PastaImage from "../resources/Logo/Pasta.png";
+import BackgroundImage from "../resources/backgrounds/Background.png";
+
+function Login() {
+    const history = useHistory();
+
     return (
+        // Andrew Nashaat
         <section className="login-route">
-            <h1>Login Page</h1>
+            <img
+                className="Logo"
+                src={LogoImage}
+                alt="Logo"
+                onClick={() => history.push("/")}
+            />
+            <img className="pesto" src={PastaImage} alt="pesto" />
+            <img className="pizza" src={PizzaImage} alt="pizza" />
+            <img
+                className="register-background"
+                src={BackgroundImage}
+                alt="register-background"
+            />
+            <p> Don't miss a delicious meal...</p>
+            <form action="" method="get">
+                <input
+                    type="email"
+                    class="input-box"
+                    placeholder="Email Address"
+                />
+                <input
+                    type="password"
+                    class="input-box"
+                    placeholder="Password"
+                />
+                <br />
+                <button type="submit" class="button">
+                    Login
+                </button>
+                <br /> <br />
+                <p> Do you want to join our family..?</p>
+                <button
+                    type="submit"
+                    class="button"
+                    onClick={() => history.push("/register")}
+                >
+                    Sign up
+                </button>
+                <br />
+            </form>
         </section>
     );
-};
+}
 
 export default Login;
